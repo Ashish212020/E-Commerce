94f2142b6755ace7455eda604156ed9a6fc1302d
Ashish212020 <tiwariashish01332@gmail.com>
2024-06-19 18:43:31 +0530

    first commit

---
+++ src/Components/DescriptionBox/DescriptionBox.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import './DescriptionBox.css'
+ 
+const DescriptionBox = () => {
+  return (
+    <div className='descriptionbox'>
+      <div className="descriptionbox-navigator">
+       
+      <div className="descriptionbox-nav-box">Description</div>
+      <div className="descriptionbox-nav-box fade">Reviews (122)</div>
+      </div>
+      <div className="descriptionbox-description">
+        <p>Discover a world of endless shopping possibilities on our cutting-edge e-commerce platform. Immerse yourself in a seamless online shopping experience, where curated collections, exclusive deals, and user-friendly navigation converge. Explore a diverse range of products, from fashion and electronics to home essentials, all delivered with convenience at your fingertips. Elevate your shopping journey with us, where quality meets convenience in every click.</p>
+        <p>Indulge in personalized recommendations tailored just for you, and enjoy secure transactions with our state-of-the-art payment system. With swift shipping and responsive customer support, redefine the way you shop on our e-commerce haven.</p>
+      </div>
+    </div>
+  )
+}
+
+export default DescriptionBox
---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import './Navbar.css'
+import logo from '../Assets/logo.png'
+import cart_icon from '../Assets/cart_icon.png'
+import { useState } from 'react'
+import { Link } from 'react-router-dom'
+const Navbar = () => {
+  const [menu,setMenu]=useState("Shop");
+  return (
+    <div className='navbar'>
+      <div className="nav-logo">
+        <img src={logo} alt="" />
+        <p>SHOPPER</p>
+      </div>
+      <ul className="nav-menu">
+        <li onClick={()=>{setMenu("Shop")}}><Link to='/'style={{textDecoration:'none'}}>Shop</Link>{menu==="Shop"?<hr/>:<></>}</li>
+        <li onClick={()=>{setMenu("Men")}}><Link to='/Mens'style={{textDecoration:'none'}}>Men</Link> {menu==="Men"?<hr/>:<></>}</li>
+        <li onClick={()=>{setMenu("Women")}}><Link to='/Women'style={{textDecoration:'none'}}>Women</Link>{menu==="Women"?<hr/>:<></>}</li>
+        <li onClick={()=>{setMenu("Kids")}}><Link to='/Kids'style={{textDecoration:'none'}}>Kids</Link>{menu==="Kids"?<hr/>:<></>}</li>
+      </ul>
+      <div className="nav-login-cart">
+        <Link to='/login'><button>Login</button></Link>
+        <Link to='/cart'><img src={cart_icon} alt=""/></Link> 
+        <div className="nav-cart-count">0</div>
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
